d1bab8dfc719f3001ac97d22d423a07b3a00f744
park <ckdwls2525@naver.com>
2019-06-03 19:29:44 +0900

    added a logger(morgan), nodemon and error handling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+
 // app.use((req, res) => {
 //     res.status(200).json({
 //         message: 'It'
@@ -11,10 +13,26 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
 
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            msg: error.message
+        }
+    });
+});
 
 
 const PORT = 3000;
