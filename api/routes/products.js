b4eb1202c280cee39cfa16e16073d1690a119521
park <ckdwls2525@naver.com>
2019-06-03 20:56:39 +0900

    product database functions2

--- api/routes/products.js
+++ api/routes/products.js
@@ -23,6 +23,13 @@ router.get('/', (req, res) => {
     // });
 });
 
+
+
+
+
+
+
+
 // 데이터 생성
 router.post('/', (req, res) => {
 
@@ -56,17 +63,78 @@ router.post('/', (req, res) => {
 });
 
 // 데이터 수정
-router.patch('/', (req, res) => {
-    res.status(200).json({
-        msg: '데이터 수정 성공!!'
-    });
+router.patch('/:productId', (req, res) => {
+    
+    const id = req.params.productId;
+    const updateOps = {};
+
+    for(const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+
+    productModel
+        .update({_id: id}, { $set: updateOps })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    // res.status(200).json({
+    //     msg: '데이터 수정 성공!!'
+    // });
 });
 
-router.delete('/', (req,res) => {
-    res.status(200).json({
-        msg: '데이터 삭제 성공!!'
-    });
+// 데이터 삭제
+router.delete('/:productId', (req,res) => {
+
+    const id = req.params.productId;
+    productModel
+        .remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    // res.status(200).json({
+    //     msg: '데이터 삭제 성공!!'
+    // });
 });
+
+// 상세데이터 불러오기
+router.get('/:productId', (req,res) => {
+
+    const id = req.params.productId;
+    productModel
+        .findById(id)
+        .exec()
+        .then(doc => {
+            console.log("From database", doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({
+                    msg: "관련 제품이 없음"
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
 
 
 module.exports = router;
