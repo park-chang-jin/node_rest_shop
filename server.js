fb5afa53a2562ddac1bcb768f936c670bb421842
park <ckdwls2525@naver.com>
2019-06-03 20:10:02 +0900

    added mongooes

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
 // app.use((req, res) => {
 //     res.status(200).json({
 //         message: 'It'
@@ -19,9 +21,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+const db = "mongodb+srv://chang:ckdwls0511!@cluster0-kbuny.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(db, { useNewUrlParser: true})
+    .then( () => console.log("MongoDB Connected ..."))
+    .catch(err => console.log(err));
+
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
-
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
