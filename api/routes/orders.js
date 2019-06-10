5ac80e971e4782386a3c1552e3c1d87f53210044
park <ckdwls2525@naver.com>
2019-06-10 19:52:07 +0900

    order get all, detail, delete

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -6,9 +6,40 @@ const productModel = require("../models/product");
 
 // 데이터 불러오기
 router.get('/', (req, res) => {
-    res.status(200).json({
-        msg: 'order data 데이터 불러옴!!'
-    });
+    // res.status(200).json({
+    //     msg: 'order data 데이터 불러옴!!'
+    // });
+
+    orderModel
+        .find()
+        .select("product quantity _id")
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+
+                count: docs.length,
+                order: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:3000/orders/" + doc._id
+                        }
+                    };
+                })
+
+            });
+           
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
 });
 
 router.post('/', (req, res) => {
@@ -61,11 +92,60 @@ router.patch('/', (req, res) => {
     });
 });
 
-router.delete('/', (req, res) => {
-    res.status(200).json({
-        msg: 'order data 데이터 삭제!!'
-    });
+router.delete('/:orderId', (req, res) => {
+    // res.status(200).json({
+    //     msg: 'order data 데이터 삭제!!'
+    // });
+    const id = req.params.orderId;
+    orderModel
+        .remove({_id: id})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                msg: "success delete",
+                reqeust: {
+                    type: "POST",
+                    url: "http://localhost:3000/orders",
+                    body: {productId: String, quantity: Number}
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
+// 상세데이터 불러오기
+router.get("/:orderId", (req, res) => {
+
+    const id = req.params.orderId;
+    orderModel
+        .findById(id)
+        .exec()
+        .then(order => {
+            if(!order) {
+                return res.status(404).json({
+                    msg: "Order not found"
+                });
+            }
+            res.status(200).json({
+                order: order,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/orders"
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
+});
 
 module.exports = router;
