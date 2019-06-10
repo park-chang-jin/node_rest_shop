b2e95d48218bf14abccbe49a80136a5765b83499
park <ckdwls2525@naver.com>
2019-06-10 20:48:58 +0900

    login compare

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userModel = require("../models/users");
-
 
 
 // 회원가입
@@ -17,7 +17,6 @@ router.post('/signup', (req, res) => {
                 return res.status(409).json({
                     msg: "Mail exists"
                 });
-
             } else {
                 bcrypt.hash(req.body.password, 10, (err, hash) => {
                     if(err) {
@@ -45,20 +44,54 @@ router.post('/signup', (req, res) => {
                                     error: err
                                 });
                             });
-
                     }
                 });
             }         
         })
     
-   
-
-    
-
 });
 
 // 로그인
 router.post('/login', (req, res) => {
+
+    userModel
+        .find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if(user.length < 1) {
+                return res.status(401).json({
+                    msg: "Not userinfo"
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err) {
+                    return res.status(400).json({
+                        msg: "password not match"
+                    });
+                }
+                if(result) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    },
+                    "secret", { expiresIn: "1h" }
+                    );
+                    return res.status(200).json({
+                        msg: "Auth sucessful",
+                        token: token
+                    });
+                }
+                res.status(401).json({
+                    msg: "Auth failed"
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
     
 });
 
