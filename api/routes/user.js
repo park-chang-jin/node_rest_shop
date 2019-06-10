f8fd97648e435fcdc52cac553c5817afdad9eab8
park <ckdwls2525@naver.com>
2019-06-10 20:25:36 +0900

    email exist and email '@' type

--- api/routes/user.js
+++ api/routes/user.js
@@ -9,36 +9,49 @@ const userModel = require("../models/users");
 
 // 회원가입
 router.post('/signup', (req, res) => {
-    
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err) {
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-            const user = new userModel({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
-            });
 
-            user
-                .save()
-                .then(result => {
-                    res.status(200).json({
-                        msg: "user created",
-                        userinfo: result
-                    });
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
-                    });
+    userModel.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    msg: "Mail exists"
                 });
 
-        }
-    });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if(err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new userModel({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+
+                        user
+                            .save()
+                            .then(result => {
+                                res.status(200).json({
+                                    msg: "user created",
+                                    userinfo: result
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+
+                    }
+                });
+            }         
+        })
+    
+   
 
     
 
